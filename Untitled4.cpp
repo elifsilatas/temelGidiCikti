6e702412d4fac891f3d393e67d5444a8015216d3
Elif Sıla Taş <122424595+elifsilatas@users.noreply.github.com>
2023-12-03 19:30:29 +0300

    temelGirdiCikti

---
+++ Untitled4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	
+	cout << "temel girdi cikti islemleri" << endl;
+	
+	int sayi;
+	
+	cin >> sayi; //klavyeden girilecek deðer.
+	
+	cout << "klavyeden girilen deger: " << sayi << endl;
+	cout << "klavyeden girilen degerin 100 fazlasi: " << sayi+100 << endl;
+	
+}
